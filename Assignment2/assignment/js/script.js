8f0fc9cefdbdea834a8d2e6df157f22dd95f6f2f
hornoo <rchrdhorne1@gmail.com>
2016-04-26 15:29:04 +1200

    working on capturing form content

--- Assignment2/assignment/js/script.js
+++ Assignment2/assignment/js/script.js
@@ -1,1 +1,14 @@
 //your code for Assignment 2 goes here
+
+$(document).ready(function(){
+
+
+    $('btn-estimate').on("click", processOrder);
+
+});
+
+function processOrder() {
+
+    return true;
+
+}
